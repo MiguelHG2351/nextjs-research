733a327dfea04e80bce1af7b079ab3bb32317ed4
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-07-12 04:35:33 +0000

    setup next-auth

--- lib/Providers.tsx
+++ lib/Providers.tsx
@@ -2,10 +2,21 @@
 
 /* Core */
 import { Provider } from 'react-redux'
+import { SessionProvider } from 'next-auth/react'
 
 /* Instruments */
 import { store } from '@/lib/redux'
 
-export const Providers = (props: React.PropsWithChildren) => {
-  return <Provider store={store}>{props.children}</Provider>
+type Props = React.PropsWithChildren<{
+  session: any
+}>
+
+export const Providers = (props: Props) => {
+  return (
+    <SessionProvider session={props.session}>
+      <Provider store={store}>
+        {props.children}
+      </Provider>
+    </SessionProvider>
+  )
 }
