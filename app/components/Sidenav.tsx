cff672667b348b4b249b02ffbadfe4222d3fb725
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-07-19 06:41:22 -0600

    add logout link

--- app/components/Sidenav.tsx
+++ app/components/Sidenav.tsx
@@ -2,7 +2,8 @@
 
 import Link from "next/link";
 import HireForemostIcon from "./icons/HireForemostIcon";
-import { useSession } from "next-auth/react";
+
+// import { useSession } from "next-auth/react";
 
 const links = [
   {
@@ -28,8 +29,8 @@ const links = [
 ]
 
 export default function Sidenav() {
-  const session = useSession();
-  console.log(session)
+  // const session = useSession();
+  // console.log(session)
   
   return (
     <section className="border-r border-r-gray-100 bg-gray-50 px-4">
@@ -73,7 +74,7 @@ export default function Sidenav() {
           </Link>
           <a
             href="https://github.com/MiguelHG2351/nextjs-research"
-            className="my-1 flex gap-1 py-1 px-2 pr-16 text-[length:14px]">
+            className="my-1 flex gap-1 py-1 px-2 text-[length:14px]">
             GitHub{" "}
             <svg width={24} height={24} fill="none">
               <path
@@ -85,24 +86,18 @@ export default function Sidenav() {
               />
             </svg>
           </a>
-          <form
-            method="post"
-            action="/logout"
-            encType="application/x-www-form-urlencoded"
-            className="my-1 py-1 px-2 pr-16 text-[length:14px]">
-            <button type="submit" className="flex gap-1 font-bold">
-              Logout{" "}
-              <svg width={24} height={24} fill="none">
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  d="m15.75 8.75 3.5 3.25-3.5 3.25M19 12h-8.25M15.25 4.75h-8.5a2 2 0 0 0-2 2v10.5a2 2 0 0 0 2 2h8.5"
-                />
-              </svg>
-            </button>
-          </form>
+          <a href="/api/auth/signout" className="my-1 flex gap-1 py-1 px-2 text-[length:14px]">
+            Logout{" "}
+            <svg width={24} height={24} fill="none">
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="m15.75 8.75 3.5 3.25-3.5 3.25M19 12h-8.25M15.25 4.75h-8.5a2 2 0 0 0-2 2v10.5a2 2 0 0 0 2 2h8.5"
+              />
+            </svg>
+          </a>
         </div>
       </div>
     </section>
